848a7a407bc3173688f88a091e1b68925b3c707d
nawszera <kkowalskigustavo@gmail.com>
2025-05-19 20:41:00 -0300

    project created

---
+++ src/test/java/project/games/personal/PersonalApplicationTests.java
@@ -0,0 +1,13 @@
+package project.games.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
