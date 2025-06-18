9d10ef553206a3c9fb80371222a43dfa2084daeb
Gustavo <kkowakskigustavo@gmail.com>
2025-06-18 00:28:12 -0300

    FindAll gamelist

--- src/main/java/project/games/personal/controller/GameListController.java
+++ src/main/java/project/games/personal/controller/GameListController.java
@@ -2,6 +2,7 @@ package project.games.personal.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import project.games.personal.dto.GameListDTO;
 import project.games.personal.dto.GameMinDTO;
 import project.games.personal.dto.ReplacementDTO;
 import project.games.personal.service.GameListService;
@@ -16,6 +17,12 @@ public class GameListController {
     private GameListService gameListService;
     @Autowired
     private GameService gameService;
+
+    @GetMapping
+    public List<GameListDTO> findALl(){
+        return gameListService.findAll();
+
+    }
 
     @GetMapping(value = "/{listId}/games")
     public List<GameMinDTO> findByList(@PathVariable Long listId){
