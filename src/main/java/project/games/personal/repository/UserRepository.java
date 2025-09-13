8a376d08ee2eba5f121b9e756693fbf9a6c4f73a
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-09-12 23:48:27 -0300

    feat(service): implement UserService implementing UserDetailsService

--- src/main/java/project/games/personal/repository/UserRepository.java
+++ src/main/java/project/games/personal/repository/UserRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import project.games.personal.entities.User;
 
+import java.util.Optional;
+
 @Service
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
