d3d81dfa7f9c6084a7ebea72715e80f8b8ca9357
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-09-14 15:32:10 -0300

    feat(handler): add handler to ConflictException

--- src/main/java/project/games/personal/exception/handler/ResourceExceptionHandler.java
+++ src/main/java/project/games/personal/exception/handler/ResourceExceptionHandler.java
@@ -1,10 +1,28 @@
 package project.games.personal.exception.handler;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import project.games.personal.exception.ConflictException;
+import project.games.personal.exception.dto.StandardError;
+
+import java.time.Instant;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<StandardError> conflict(ConflictException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.CONFLICT;
+        StandardError err = new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(status.value());
+        err.setError("Conflict");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
 
 }
