b815ff803555fedcb08cb20db2af3304ac60efc3
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-09-17 20:15:03 -0300

    feat(handler): add handler to MethodArgumentNotValidException error

--- src/main/java/project/games/personal/exception/handler/ResourceExceptionHandler.java
+++ src/main/java/project/games/personal/exception/handler/ResourceExceptionHandler.java
@@ -3,11 +3,14 @@ package project.games.personal.exception.handler;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import project.games.personal.exception.ConflictException;
 import project.games.personal.exception.ResourceNotFoundException;
 import project.games.personal.exception.dto.StandardError;
+import project.games.personal.exception.dto.ValidationError;
 
 import java.time.Instant;
 
@@ -16,15 +19,32 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<StandardError> handleConflict(ConflictException e, HttpServletRequest request) {
-        return buildResponse(HttpStatus.CONFLICT, e, request);
+        return buildStandardResponse(HttpStatus.CONFLICT, e, request);
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> handleNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-        return buildResponse(HttpStatus.NOT_FOUND, e, request);
+        return buildStandardResponse(HttpStatus.NOT_FOUND, e, request);
     }
 
-    private ResponseEntity<StandardError> buildResponse(HttpStatus status, Exception e, HttpServletRequest request) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> handleValidationError(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(
+                Instant.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                e.getMessage(),
+                request.getRequestURI()
+        );
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+    private ResponseEntity<StandardError> buildStandardResponse(HttpStatus status, Exception e, HttpServletRequest request) {
         StandardError error = new StandardError(
                 Instant.now(),
                 status.value(),
@@ -34,6 +54,8 @@ public class ResourceExceptionHandler {
         );
         return ResponseEntity.status(status).body(error);
     }
+
+
 }
 
 
