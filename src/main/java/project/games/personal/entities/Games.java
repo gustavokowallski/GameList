577056d6b459c84384603d42b380c29dc599ad0f
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-09-18 00:46:26 -0300

    refactor(games): Remove setters from games class

--- src/main/java/project/games/personal/entities/Games.java
+++ src/main/java/project/games/personal/entities/Games.java
@@ -40,76 +40,40 @@ public class Games {
         this.longDescription = longDescription;
     }
 
-    public String getTitle() {
-        return title;
+    public Long getId() {
+        return id;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
+    public String getTitle() {
+        return title;
     }
 
     public Integer getYear() {
         return year;
     }
 
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getGenre() {
         return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public Double getScore() {
-        return this.score;
-    }
-
-    public void setScore(Double score) {
-        this.score = score;
     }
 
     public String getPlatforms() {
         return platforms;
     }
 
-    public void setPlatforms(String platforms) {
-        this.platforms = platforms;
+    public Double getScore() {
+        return score;
     }
 
     public String getImgUrl() {
         return imgUrl;
     }
 
-    public void setImgUrl(String imgUrl) {
-        this.imgUrl = imgUrl;
-    }
-
-    public String getLongDescription() {
-        return longDescription;
-    }
-
-    public void setLongDescription(String longDescription) {
-        this.longDescription = longDescription;
-    }
-
     public String getShortDescription() {
         return shortDescription;
     }
 
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
+    public String getLongDescription() {
+        return longDescription;
     }
 
     @Override
